0060f89b83c5485cbaa16021f1ea606001d629e9
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-15 09:41:45 +0900

    미션3

--- first/src/com/kita/first/mission/Mission3.java
+++ first/src/com/kita/first/mission/Mission3.java
@@ -1,5 +1,7 @@
 package com.kita.first.mission;
 
+import java.util.Scanner;
+
 public class Mission3 {
 	/*
 	  	성별을 입력해 주세요: (남/여)
@@ -14,6 +16,38 @@ public class Mission3 {
 	  
 	 */
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("성별을 입력해 주세요: (남/여)");
+		String gender = scan.nextLine();
+		System.out.print("키를 입력해 주세요:");
+		int height = scan.nextInt();
 		
+		if("남".equals(gender)) {
+			
+			if(height > 160) {
+				System.out.println("평균 초과입니다.");
+			} else if(height < 160) {
+				System.out.println("평균 미만입니다.");
+			} else {
+				System.out.println("평균입니다.");
+			}
+			
+			
+		} else if("여".equals(gender)) {
+			
+			if(height > 150) {
+				System.out.println("평균 초과입니다.");
+			} else if(height < 150) {
+				System.out.println("평균 미만입니다.");
+			} else {
+				System.out.println("평균입니다.");
+			}
+			
+		} else {
+			System.out.println("성별을 잘 못 입력하셨습니다.");
+		}
 	}
 }
+
+
+
---
+++ first/src/com/kita/first/mission/Mission3_1.java
@@ -0,0 +1,41 @@
+package com.kita.first.mission;
+
+import java.util.Scanner;
+
+public class Mission3_1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("성별을 입력해 주세요: (남/여)");
+		String gender = scan.nextLine();
+		
+		if("남".equals(gender) || "여".equals(gender)) {
+			
+			System.out.print("키를 입력해 주세요:");
+			int height = scan.nextInt();
+			
+			int standHeight = 0;
+			
+			if("남".equals(gender)) {			
+				standHeight = 160;
+			} else {
+				standHeight = 150;
+			}
+						
+			if(height > standHeight) {
+				System.out.println("평균 초과입니다.");
+			} else if(height < standHeight) {
+				System.out.println("평균 미만입니다.");
+			} else {
+				System.out.println("평균입니다.");
+			}
+			
+		} else {
+			System.out.println("성별을 잘 못 입력하셨습니다.");
+		}
+		
+		
+
+	}
+
+}
