a2781ecc7cb4d327bdbf903a647d56a69e9bcace
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-17 17:47:49 +0900

    6-17 끝

--- first/src/com/kita/first/mission/GreatMission3.java
+++ first/src/com/kita/first/mission/GreatMission3.java
@@ -9,6 +9,26 @@ public class GreatMission3 {
 				{98, 65, 87} //국어
 		};
 		
+		String[] clsArr = {"영어", "수학", "국어"};
+		int[] sumArr = new int[clsArr.length];
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int z=0; z<arr[i].length; z++) {
+				sumArr[i] += arr[i][z];
+			}
+		}
+		
+		int totalSum = 0;
+		int totalCnt = 0;
+		for(int i=0; i<sumArr.length; i++) {
+			System.out.printf("%s합계: %d, 평균: %.1f\n"
+					, clsArr[i], sumArr[i], (float)sumArr[i]/arr[i].length);
+			totalSum += sumArr[i];
+			totalCnt += arr[i].length;
+		}
+		
+		System.out.printf("전체합계: %d, 평균: %.1f\n", totalSum, (float)totalSum/totalCnt);
+		
 		/*
 			영어합계: ?, 평균: ?
 			수학합계: ?, 평균: ?
