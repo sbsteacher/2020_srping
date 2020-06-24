091ca2e513aa0c1329193a33e3d4bf1aeba41298
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-24 11:09:31 +0900

    MyListTest(중간)

--- first/src/com/kita/first/level3/MyListTest.java
+++ first/src/com/kita/first/level3/MyListTest.java
@@ -1,17 +1,21 @@
 package com.kita.first.level3;
 
+import java.util.Arrays;
+
 public class MyListTest {
 	public static void main(String[] args) {
 		MyList list = new MyList();
 		list.add(10);
 		list.add(15);
 		list.add(20);
-	
-		list.remove(0); //1번 인덱스 방 삭제
+		list.add(1, 1200);
+		
+		list.remove();
+		
 		for(int i=0; i<list.size(); i++) {
 			System.out.print(list.get(i) + ", ");
 		}
-		//ex) 1,2,3,4 > 1,3,4
+		
 		
 	}
 }
@@ -35,33 +39,30 @@ class MyList {
 		return arr[idx];
 	}
 	
-	void remove(int idx) {
+	int remove(int idx) {
 		int[] temp = new int[arr.length - 1];
 		for(int i=0; i<temp.length; i++) {
-			if(i < idx) {
-				temp[i] = arr[i];
-			} else {
-				temp[i] = arr[i+1];
-			}
+			temp[i] = arr[i < idx ? i : i + 1];
 		}
+		int delVal = arr[idx];
 		arr = temp;
+		return delVal;
+	}	
+	int remove() {
+		return remove(arr.length - 1);
 	}
 	
-	void remove() {
-		int[] temp = new int[arr.length - 1];
-		for(int i=0; i<temp.length; i++) {
-			temp[i] = arr[i];
-		}
+	void add(int idx, int num) {
+		int[] temp = new int[arr.length + 1];		
+		for(int i=0; i<arr.length; i++) {
+			temp[i < idx ? i : i + 1] = arr[i];
+		}		
+		temp[idx] = num;
 		arr = temp;
 	}
 	
 	void add(int num) {
-		int[] temp = new int[arr.length + 1];
-		for(int i=0; i<arr.length; i++) {
-			temp[i] = arr[i];
-		}
-		temp[arr.length] = num;
-		arr = temp;
+		add(arr.length, num);
 	}
 }
 
