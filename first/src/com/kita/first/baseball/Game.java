2112ef470457a76a2a197fabb656861bb64b5191
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-26 09:35:37 +0900

    숫자야구게임 (객체지향) 마무으리 ver2

--- first/src/com/kita/first/baseball/Game.java
+++ first/src/com/kita/first/baseball/Game.java
@@ -4,6 +4,12 @@ public class Game {
 
 	public static void main(String[] args) {
 		int gameCnt = 3;
+		if(args.length != 0) {
+			try {
+				gameCnt = Integer.parseInt(args[0]);
+			} catch(Exception e) {}
+		}
+		
 		Baseball ball = new Baseball(gameCnt);
 		MyBall myBall = new MyBall(gameCnt);
 		
